d3635fb27e23961ee0b88247d22e1f31d3c9b6b3
Alexey <alexh2@yandex.ru>
2022-09-19 12:49:48 +0300

    Solutionm

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,6 +1,10 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Deпmark", "Kazan"] -> []
 
 using System;
 using static System.Console;
@@ -11,10 +15,12 @@ string[] array = AskArray();
 string[] result = FindLessThan(array, 3);
 WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
 
-string[] FindLessThan(string[] input, int n) {
+string[] FindLessThan(string[] input, int n)
+ {
     string[] output = new string[CountLessThan(input, n)];
 
-    for(int i = 0, j = 0; i < input.Length; i++) {
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
         if(input[i].Length <= n)
          {
             output[j] = input[i];
@@ -25,7 +31,8 @@ string[] FindLessThan(string[] input, int n) {
     return output;
 }
 
-int CountLessThan(string[] input, int n) {
+int CountLessThan(string[] input, int n)
+ {
     int count = 0;
 
     for(int i = 0; i < input.Length; i++)
